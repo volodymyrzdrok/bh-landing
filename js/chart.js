c27b9ae4d205adcf5b9cd5f46e319251231dbe92
Volodymyr Zdrok <v.zdrok@skgp.eu>
2024-02-18 13:51:44 +0200

    added gradien

--- js/chart.js
+++ js/chart.js
@@ -1,63 +1,97 @@
-// Дані для графіку
-const data = {
-    labels: ["2016", "2017", "2018", "2019", "2020", "2021", "2022", "2023"],
-    datasets: [{
-      data: [17145, 35929, 47219, 131700, 132696, 243273,340388,451878],
-      borderColor: 'linear-gradient(90deg,#f39200 0%,#f4a90c 20%,#f7d01f 48%,#6d100a 100%)', // Колір лінії графіку
-      backgroundColor: 'linear-gradient(90deg,#f39200 0%,#f4a90c 20%,#f7d01f 48%,#6d100a 100%)', // Прозорий фон
-      borderWidth: 2,
-      pointBackgroundColor: '#000',
-      pointBorderColor: '#000',
-      pointHoverBackgroundColor: '#000',
-      pointHoverBorderColor: '#000',
-    }]
-  };
-  
-  // Налаштування графіку
-  const config = {
-    type: 'line',
-    data: data,
-    options: {
-      scales: {
-        x: {
-          display: true,
-          grid: {
-            display: false
-          },
-      
-        },
-        y: {
-          display: true,
-          grid: {
-            display: false
-          },
-          title: {
-            display: true,
-            text: 'Кількість гостей Black Honey'
-          }
-        }
-      },
-      plugins: {
-        legend: {
-          display: false
-        },
-        fillRectLinearGradient: true // Активувати плагін для градієнту
-      },
-      layout: {
-        padding: {
-          left: 0,
-          right: 0,
-          top: 0,
-          bottom: 0
-        }
-      },
-    }
-  };
-  
-  // Ініціалізація графіку
-  const myChart = new Chart(
-    document.getElementById('myChart'),
-    config
-  );
+let width, height, gradient;
 
-  
+function getGradient(ctx, chartArea) {
+  const chartWidth = chartArea.right - chartArea.left;
+  const chartHeight = chartArea.bottom - chartArea.top;
+  if (!gradient || width !== chartWidth || height !== chartHeight) {
+    width = chartWidth;
+    height = chartHeight;
+    gradient = ctx.createLinearGradient(
+      chartArea.left,
+      chartArea.bottom,
+      chartArea.left,
+      chartArea.top
+    );
+    gradient.addColorStop(0, "#FFB800B8");
+    gradient.addColorStop(0.2, "#FA8B08B8");
+    gradient.addColorStop(0.48, "#FA0808A3");
+    gradient.addColorStop(1, "#646006FC");
+  }
+
+  return gradient;
+}
+
+const data = {
+  labels: ["2016", "2017", "2018", "2019", "2020", "2021", "2022", "2023"],
+  datasets: [
+    {
+      data: [17145, 35929, 47219, 131700, 132696, 243273, 340388, 451878],
+
+      borderColor: function (context) {
+        const chart = context.chart;
+        const { ctx, chartArea } = chart;
+
+        if (!chartArea) {
+          return;
+        }
+        return getGradient(ctx, chartArea);
+      },
+      borderWidth: 5,
+      // fill: false,
+      // cubicInterpolationMode: "monotone",
+      tension: 0.4,
+
+      pointBackgroundColor: "#000",
+      pointBorderColor: "#000",
+      pointHoverBackgroundColor: "#000",
+      pointHoverBorderColor: "#000",
+      pointRadius: 7,
+    },
+  ],
+};
+
+const config = {
+  type: "line",
+  data: data,
+  options: {
+    responsive: true,
+    plugins: {
+      title: {
+        display: false,
+      },
+    },
+    interaction: {
+      intersect: false,
+    },
+    scales: {
+      x: {
+        display: true,
+        grid: {
+          display: false,
+        },
+        title: {
+          display: true,
+        },
+      },
+      y: {
+        display: true,
+        grid: {
+          display: false,
+        },
+        title: {
+          display: true,
+          text: "Кількість гостей Black Honey",
+        },
+        suggestedMin: 0,
+        suggestedMax: 100,
+      },
+    },
+    plugins: {
+      legend: {
+        display: false,
+      },
+    },
+  },
+};
+
+const myChart = new Chart(document.getElementById("myChart"), config);
