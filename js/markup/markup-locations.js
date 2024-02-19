4ae067551ab411c937905308748cb1f06321ec87
Volodymyr Zdrok <v.zdrok@skgp.eu>
2024-02-19 16:56:32 +0200

    added priviusly modal without content

--- js/markup/markup-locations.js
+++ js/markup/markup-locations.js
@@ -1,7 +1,7 @@
 export function markupLocations(data) {
   return data.map(
-    ({ city }) => `
-<li class="d-flex align-items-start justify-content-center m-3 location__item">
+    ({id, city }) => `
+<li class="d-flex align-items-start justify-content-center m-3 location__item" data-id="${id}">
 <img src="images/map-pin.svg" class="location__pin pe-1 pe-lg-4" alt="pin" />
 <p class="text-white location__name" data-i18n=${city}></p>
 </li>
--- js/render-locations.js
+++ js/render-locations.js
@@ -1,10 +1,50 @@
-import { locations } from './db/locations.js';
-import { markupLocations } from './markup/markup-locations.js';
-
+import { locations } from "./db/locations.js";
+import { markupLocations } from "./markup/markup-locations.js";
 
 $(document).ready(function () {
-  const containerLocations = $('.location__list');
+  const containerLocations = $(".location__list");
   if (containerLocations) {
     containerLocations.html(markupLocations(locations));
   }
-})
+
+
+
+
+//   $("#locationModal").on("show.bs.modal", function () {
+//     $("body").addClass("modal-open-noscroll");
+//   });
+
+//   $("#locationModal").on("hidden.bs.modal", function () {
+//     $("body").removeClass("modal-open-noscroll");
+//   });
+
+
+
+
+
+  //modal
+  $("#locationModal").modal("show");
+
+  $(".location__item").click(function (e) {
+    const id = $(this).data("id");
+    console.log("id", id);
+
+    $("#locationModal").modal("show");
+  });
+
+
+
+
+  $(".location-item:first").addClass('active');
+  $(".location-content p:first").show();
+
+
+  $(".location-item").hover(function(){
+    var targetId = $(this).data("target");
+    $(".location-content p").hide();
+    $(".location-content p[data-id='" + targetId + "']").show();
+    $(".location-item").removeClass("active");
+    $(this).addClass("active");
+  });
+ 
+});
