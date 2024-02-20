d83c49eebc60d6e13c11e687d4ea885138bb0f0b
Volodymyr Zdrok <v.zdrok@skgp.eu>
2024-02-20 10:50:34 +0200

    added adress link in modal

--- js/markup/markup-modal-locations.js
+++ js/markup/markup-modal-locations.js
@@ -1,34 +1,40 @@
 export function markupModalLocations(data) {
-    const {city,address} = data;
-  return`
+  const { city, address } = data;
+  return `
     <div class="row">
       <div class="col-4 p-4 pe-1">
         <h3 class="locModal__title fw-m mb-3" data-i18n=${city} ></h3>
     <div>
-    ${address.map(
-      ({
-        id,
-        name,
-      }) => `<p class="locModal-name__item" data-target=${id} data-i18n=${name}>
+    ${address
+      .map(
+        ({
+          id,
+          name,
+          url,
+        }) => `<a class="locModal-name__item" href="https://maps.app.goo.gl/${url}" data-target=${id} data-i18n=${name}>
     
-  </p>`
-    ).join('')}
+  </a>`
+      )
+      .join("")}
       
-    
     </div>
       </div>
       <div class="col-8">
         <div class="locModal-content">
-        ${address.map(
-          ({ id, name }) => `<div class="locModal-content__item" data-id=${id}>
+        ${address
+          .map(
+            ({
+              id,
+              name,
+            }) => `<div class="locModal-content__item" data-id=${id}>
         <img src="./images/locations/${name}.jpg" alt="${name}" />
       </div>`
-        ).join('')}
+          )
+          .join("")}
 
           
       
         </div>
       </div>
-    </div>`
- 
+    </div>`;
 }
