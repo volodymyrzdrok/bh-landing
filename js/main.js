5ec7838df4efb1c5a96253bf1bae5b0af5d70fc7
Volodymyr Zdrok <v.zdrok@skgp.eu>
2024-02-21 22:30:09 +0200

    fixed trable w/ btn_link_to form

--- js/main.js
+++ js/main.js
@@ -1,13 +1,11 @@
 import "./render/render-locations.js";
 import "./questions-block.js";
 import "./render/footer.js";
-import "./localization.js";
 import "./swiper.js";
+import "./localization.js";
 import "./chart.js";
 import "./utils/animations.js";
-import "./utils/trigger-element.js";
 import "./navbar.js";
 import "./utils/modal-ask.js"
 
-   
 
--- js/utils/trigger-element.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function(){
-    $('[data-i18n="link_to_form"]').trigger('focus');
-    $('[data-i18n="link_to_form"]').trigger('hover');
-});
