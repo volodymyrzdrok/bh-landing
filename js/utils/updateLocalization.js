9ba9879f7dec2d269ede6a6daa97f6d0f0024cc4
Volodymyr Zdrok <v.zdrok@skgp.eu>
2024-02-21 03:02:53 +0200

    fixed localization for selects

--- js/utils/updateLocalization.js
+++ js/utils/updateLocalization.js
@@ -1,16 +1,20 @@
 export function updateContent() {
-  $('[data-i18n]').each(function() {
-    const key = $(this).data('i18n');
+  $("[data-i18n]").each(function () {
+    const key = $(this).data("i18n");
     const tagName = this.tagName.toLowerCase();
-    
-    if (tagName === 'select') {
-      $(this).attr('data-placeholder', i18next.t(key));
-      $(document).ready(function () {
-        $(".select2").select2({
-          minimumResultsForSearch: -1,
-        });
+
+    if (tagName === "select") {
+      $(this).attr("data-placeholder", i18next.t(key));
+
+      $(".select2").select2({
+        minimumResultsForSearch: -1,
       });
-    } else if (tagName === 'option') {
+
+      $(this)
+        .parents(".select__container")
+        .find(".select2-selection__placeholder")
+        .text(i18next.t(key));
+    } else if (tagName === "option") {
       $(this).text(i18next.t(key));
     } else {
       $(this).html(i18next.t(key));
