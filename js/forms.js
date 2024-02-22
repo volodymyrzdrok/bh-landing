92565f8c72b5b8fc327f4cc00d545e22f45c34ae
Volodymyr Zdrok <v.zdrok@skgp.eu>
2024-02-22 21:25:58 +0200

    fixed js code

--- js/forms.js
+++ js/forms.js
@@ -1,13 +1,12 @@
 // const handleSubmit = (event) => {
 //     event.preventDefault();
-  
+
 //     const myForm = event.target;
 //     const formData = new FormData(myForm);
-    
+
 //     for (var pair of formData.entries()) {
 //         console.log(pair[0] + ': ' + pair[1]);
 //     }
-    
 
 //     fetch("/", {
 //       method: "POST",
@@ -20,29 +19,37 @@
 //     })
 //       .catch((error) => alert(error));
 //   };
-  
+
 //   document
 //     .querySelector("#askModal-form")
 //     .addEventListener("submit", handleSubmit);
-  
 
 const handleSubmit = (event) => {
-    event.preventDefault();
-  
-    const myForm = event.target;
-    const formData = new FormData(myForm);
+  event.preventDefault();
+
+  const myForm = event.target;
+  const formData = new FormData(myForm);
+  let text = "";
+  for (var pair of formData.entries()) {
+    //         console.log(pair[0] + ': ' + pair[1]);
+    text += pair[0] + ": " + pair[1];
     
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: new URLSearchParams(formData).toString(),
+  }
+
+  console.log('text.toString() :', text.toString())
+  fetch("/", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: text.toString(),
+  })
+    .then(() => {
+      console.log("Form successfully submitted");
+      $('#askModal').modal('hide')
+      $("#thanksModal").modal("show");
     })
-      .then(() => {console.log("Form successfully submitted");
-      $('#thanksModal').modal('show');})
-      .catch((error) => alert(error));
-  };
-  
-  document
-    .querySelector('form[name="form_question"]')
-    .addEventListener("submit", handleSubmit);
-  
+    .catch((error) => alert(error));
+};
+
+document
+  .querySelector('form[name="form_question"]')
+  .addEventListener("submit", handleSubmit);
