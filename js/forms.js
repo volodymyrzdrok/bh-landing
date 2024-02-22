5ae53d6f3041101c51bf5f70aa0752d578b2abf2
Volodymyr Zdrok <v.zdrok@skgp.eu>
2024-02-22 19:40:53 +0200

    added examples

--- js/forms.js
+++ js/forms.js
@@ -1,27 +1,48 @@
+// const handleSubmit = (event) => {
+//     event.preventDefault();
+  
+//     const myForm = event.target;
+//     const formData = new FormData(myForm);
+    
+//     for (var pair of formData.entries()) {
+//         console.log(pair[0] + ': ' + pair[1]);
+//     }
+    
+
+//     fetch("/", {
+//       method: "POST",
+//       headers: { "Content-Type": "application/x-www-form-urlencoded" },
+//       body: new URLSearchParams(formData).toString(),
+//     })
+//       .then(() => {alert("Thank you for your submission");
+//       $('#askModal').modal('hide');
+//     $('#thanksModal').modal('show');
+//     })
+//       .catch((error) => alert(error));
+//   };
+  
+//   document
+//     .querySelector("#askModal-form")
+//     .addEventListener("submit", handleSubmit);
+  
+
 const handleSubmit = (event) => {
     event.preventDefault();
   
     const myForm = event.target;
     const formData = new FormData(myForm);
     
-    for (var pair of formData.entries()) {
-        console.log(pair[0] + ': ' + pair[1]);
-    }
-    
-
     fetch("/", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: new URLSearchParams(formData).toString(),
     })
-      .then(() => {alert("Thank you for your submission");
-      $('#askModal').modal('hide');
-    $('#thanksModal').modal('show');
-    })
+      .then(() => {console.log("Form successfully submitted");
+      $('#thanksModal').modal('show');})
       .catch((error) => alert(error));
   };
   
   document
-    .querySelector("#askModal-form")
+    .querySelector("form")
     .addEventListener("submit", handleSubmit);
   
