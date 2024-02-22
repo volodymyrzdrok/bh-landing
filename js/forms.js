95bf689964045d220b096ebc2afe3b9c41b17879
Volodymyr Zdrok <v.zdrok@skgp.eu>
2024-02-22 18:53:01 +0200

    comented js for forms

--- js/forms.js
+++ js/forms.js
@@ -1,27 +1,27 @@
-const handleSubmit = (event) => {
-    event.preventDefault();
+// const handleSubmit = (event) => {
+//     event.preventDefault();
   
-    const myForm = event.target;
-    const formData = new FormData(myForm);
+//     const myForm = event.target;
+//     const formData = new FormData(myForm);
     
-    for (var pair of formData.entries()) {
-        console.log(pair[0] + ': ' + pair[1]);
-    }
+//     for (var pair of formData.entries()) {
+//         console.log(pair[0] + ': ' + pair[1]);
+//     }
     
 
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: new URLSearchParams(formData).toString(),
-    })
-      .then(() => {alert("Thank you for your submission");
-      $('#askModal').modal('hide');
-    $('#thanksModal').modal('show');
-    })
-      .catch((error) => alert(error));
-  };
+//     fetch("/", {
+//       method: "POST",
+//       headers: { "Content-Type": "application/x-www-form-urlencoded" },
+//       body: new URLSearchParams(formData).toString(),
+//     })
+//       .then(() => {alert("Thank you for your submission");
+//       $('#askModal').modal('hide');
+//     $('#thanksModal').modal('show');
+//     })
+//       .catch((error) => alert(error));
+//   };
   
-  document
-    .querySelector("#askModal-form")
-    .addEventListener("submit", handleSubmit);
+//   document
+//     .querySelector("#askModal-form")
+//     .addEventListener("submit", handleSubmit);
   
