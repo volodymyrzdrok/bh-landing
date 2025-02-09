10253ec8d3e5d3f689d172f3733f808200088ca1
Volodymyr Zdrok <volodymyrzdrok44@gmail.com>
2025-02-09 19:39:15 +0200

    added new locations and data for 2024year

--- js/db/locations.js
+++ js/db/locations.js
@@ -3,15 +3,51 @@ export const locations = [
     id: "1",
     city: "lviv",
     address: [
-      { id: "1", name: "kryva", url: "https://www.google.com/maps/place/Black+Honey/@49.8403536,24.0266014,15z/data=!4m6!3m5!1s0x473adda131ac296f:0xac8ea704792c358b!8m2!3d49.8403536!4d24.0266014!16s%2Fg%2F11m8z9lxt9?hl=uk&entry=ttu" },
-      { id: "2", name: "shota", url: "https://www.google.com/maps/place/Black+Honey/@49.8326592,24.0348958,15z/data=!4m2!3m1!1s0x0:0x78ddf15951dc9922?sa=X&ved=2ahUKEwilvpXNz8GEAxVcLRAIHfq7BqgQ_BJ6BAhTEAA&hl=uk" },
-      { id: "3", name: "galytska", url: "https://www.google.com/maps/place/Black+Honey/@49.8394649,24.0317811,15z/data=!4m2!3m1!1s0x0:0x448bcb5b2a51e31a?sa=X&ved=2ahUKEwjl64nMz8GEAxUtHxAIHdHYDJgQ_BJ6BAhaEAA&hl=uk" },
-      { id: "4", name: "shevchenka", url: "https://www.google.com/maps/place/Black+Honey/@49.8458701,24.0100572,15z/data=!4m6!3m5!1s0x473add8577142995:0x9d5435100c252dcf!8m2!3d49.8458701!4d24.0100572!16s%2Fg%2F11qyp26c5d?hl=uk&entry=ttu" },
-      { id: "5", name: "laz", url: "https://www.google.com/maps/place/Black+Honey/@49.8063998,24.0193347,15z/data=!4m6!3m5!1s0x473ae721623a1309:0xe5b2034271ceadf4!8m2!3d49.8063998!4d24.0193347!16s%2Fg%2F11l27hfv15?hl=uk&entry=ttu" },
-      { id: "6", name: "british", url: "https://www.google.com/maps/place/Black+Honey/@49.8432493,24.0061576,15z/data=!4m6!3m5!1s0x473add975b9bd3f9:0x3a9e374484549497!8m2!3d49.8432493!4d24.0061576!16s%2Fg%2F11vdf5vftj?hl=uk&entry=ttu" },
-      { id: "7", name: "parus", url: "https://www.google.com/maps/place/49%C2%B049'19.4%22N+23%C2%B059'32.7%22E/@49.8220556,23.9924167,17z/data=!3m1!4b1!4m4!3m3!8m2!3d49.8220556!4d23.9924167?hl=uk-UA&entry=ttu" },
-      { id: "8", name: "persenkivka", url: "https://www.google.com/maps/place/RAW+STUDIOS+LVIV/@49.8081073,24.0360911,15z/data=!4m2!3m1!1s0x0:0x938a817e94add361?sa=X&ved=2ahUKEwj4i5zWz8GEAxXfCBAIHbCXCNcQ_BJ6BAgPEAA" },
-      // { id: "9", name: "yaneva", url: "https://www.google.com/maps/place/Black+Honey/@49.8109506,24.0093778,15z/data=!4m6!3m5!1s0x473ae7f42b0ff401:0xe2516af3b8d1f056!8m2!3d49.8109506!4d24.0093778!16s%2Fg%2F11vdfgqhd3?hl=uk&entry=ttu" },
+      {
+        id: "1",
+        name: "kryva",
+        url: "https://www.google.com/maps/place/Black+Honey/@49.8403536,24.0266014,15z/data=!4m6!3m5!1s0x473adda131ac296f:0xac8ea704792c358b!8m2!3d49.8403536!4d24.0266014!16s%2Fg%2F11m8z9lxt9?hl=uk&entry=ttu",
+      },
+      {
+        id: "2",
+        name: "shota",
+        url: "https://www.google.com/maps/place/Black+Honey/@49.8326592,24.0348958,15z/data=!4m2!3m1!1s0x0:0x78ddf15951dc9922?sa=X&ved=2ahUKEwilvpXNz8GEAxVcLRAIHfq7BqgQ_BJ6BAhTEAA&hl=uk",
+      },
+      {
+        id: "4",
+        name: "shevchenka",
+        url: "https://www.google.com/maps/place/Black+Honey/@49.8458701,24.0100572,15z/data=!4m6!3m5!1s0x473add8577142995:0x9d5435100c252dcf!8m2!3d49.8458701!4d24.0100572!16s%2Fg%2F11qyp26c5d?hl=uk&entry=ttu",
+      },
+      {
+        id: "6",
+        name: "british",
+        url: "https://www.google.com/maps/place/Black+Honey/@49.8432493,24.0061576,15z/data=!4m6!3m5!1s0x473add975b9bd3f9:0x3a9e374484549497!8m2!3d49.8432493!4d24.0061576!16s%2Fg%2F11vdf5vftj?hl=uk&entry=ttu",
+      },
+      {
+        id: "7",
+        name: "parus",
+        url: "https://www.google.com/maps/place/49%C2%B049'19.4%22N+23%C2%B059'32.7%22E/@49.8220556,23.9924167,17z/data=!3m1!4b1!4m4!3m3!8m2!3d49.8220556!4d23.9924167?hl=uk-UA&entry=ttu",
+      },
+      {
+        id: "8",
+        name: "persenkivka",
+        url: "https://www.google.com/maps/place/RAW+STUDIOS+LVIV/@49.8081073,24.0360911,15z/data=!4m2!3m1!1s0x0:0x938a817e94add361?sa=X&ved=2ahUKEwj4i5zWz8GEAxXfCBAIHbCXCNcQ_BJ6BAgPEAA",
+      },
+      {
+        id: "3",
+        name: "zamartstynivska",
+        url: "https://www.google.com/maps/place/Black+Honey/@49.8685612,24.0247427,17z/data=!3m1!4b1!4m6!3m5!1s0x473add002f19a4cb:0x9b467a3c7f0ebafd!8m2!3d49.8685612!4d24.0247427!16s%2Fg%2F11vzqbp04l?entry=ttu&g_ep=EgoyMDI1MDIwNS4xIKXMDSoASAFQAw%3D%3D",
+      },
+      {
+        id: "5",
+        name: "chornovola",
+        url: "https://www.google.com/maps/place/Black+Honey/@49.8491077,24.0227242,17z/data=!3m1!4b1!4m6!3m5!1s0x473add004b3795ff:0xfc3b3e654343ca20!8m2!3d49.8491077!4d24.0252991!16s%2Fg%2F11vz8gc68k?hl=uk&entry=ttu&g_ep=EgoyMDI1MDIwNS4xIKXMDSoASAFQAw%3D%3D",
+      },
+      {
+        id: "9",
+        name: "leopol",
+        url: "https://www.google.com/maps/place/Black+Honey/data=!4m2!3m1!1s0x0:0x509b934c39006a0f?sa=X&ved=1t:2428&ictx=111",
+      },
     ],
   },
   // {
